eae3a876dba9e8a28f58c36e1714a897169914c5
Jamie Rodriguez <>
2022-08-08 22:52:57 +0800

    Formatting

--- src/constants.clj
+++ src/constants.clj
@@ -5,18 +5,16 @@
 
 (def player-piece-symbols ["O" "X"])
 
-(def board-area
-    (dec (bit-shift-left 1 board-size)))
+(def board-area (dec (bit-shift-left 1 board-size)))
 
 ; TO-DO: find a way to generate this dynamically for arbitrary board dimensions
-(def three-in-a-row [
-    2r001001001
-    2r010010010
-    2r100100100
-    2r000000111
-    2r000111000
-    2r111000000
-    2r100010001
-    2r001010100])
+(def three-in-a-row [2r001001001
+                     2r010010010
+                     2r100100100
+                     2r000000111
+                     2r000111000
+                     2r111000000
+                     2r100010001
+                     2r001010100])
 
 (def new-game [2r0 2r0])
